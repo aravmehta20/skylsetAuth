a82c448d6298973f03b15649c6a81ede04a96a01
aravmehta20 <aravm0720@gmail.com>
2025-05-23 16:31:45 -0500

    Update signUp.js

--- api/signUp.js
+++ api/signUp.js
@@ -1,7 +1,7 @@
 const serviceRoleKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnemZ3aGtqamt4d3NodXB3ZHNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDMwMzY0NywiZXhwIjoyMDU5ODc5NjQ3fQ.MRTSJM37ZMqG8ixq948xebNfRNc-koV-K3bsiMh2Dt0";
 const projectURL = "https://tgzfwhkjjkxwshupwdse.supabase.co";
 
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(SUPABASE_URL, SERVICE_ROLE_KEY);
 

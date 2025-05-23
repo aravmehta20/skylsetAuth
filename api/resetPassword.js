df6ab11f2421fbdf8a4837698c3df4b9ee8daf24
aravmehta20 <aravm0720@gmail.com>
2025-05-23 16:31:18 -0500

    Update resetPassword.js

--- api/resetPassword.js
+++ api/resetPassword.js
@@ -1,7 +1,7 @@
 const serviceRoleKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnemZ3aGtqamt4d3NodXB3ZHNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDMwMzY0NywiZXhwIjoyMDU5ODc5NjQ3fQ.MRTSJM37ZMqG8ixq948xebNfRNc-koV-K3bsiMh2Dt0";
 const projectURL = "https://tgzfwhkjjkxwshupwdse.supabase.co";
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(projectURL, serviceRoleKey, {
   auth: {
     autoRefreshToken: false,

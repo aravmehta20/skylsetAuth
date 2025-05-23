09a74b4e53860e5bbec68dc35957ce785a257420
aravmehta20 <aravm0720@gmail.com>
2025-05-23 15:19:26 -0500

    Update and rename auth.js to signUp.js

---
+++ api/signUp.js
@@ -0,0 +1,23 @@
+const serviceRoleKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnemZ3aGtqamt4d3NodXB3ZHNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDMwMzY0NywiZXhwIjoyMDU5ODc5NjQ3fQ.MRTSJM37ZMqG8ixq948xebNfRNc-koV-K3bsiMh2Dt0";
+const projectURL = "https://tgzfwhkjjkxwshupwdse.supabase.co";
+
+module.exports = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const { data, error } = await supabase.auth.admin.createUser({
+    email,
+    email_confirm: true,
+    password,
+    user_metadata: { 
+      name, 
+      saved_opportunities: {} 
+                   }
+  });
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  return res.status(200).json(data);
+};
+
+

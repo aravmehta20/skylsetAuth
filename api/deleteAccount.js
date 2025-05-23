4731dbd77824373fc8da0e7cad38adb0008b7a3d
aravmehta20 <aravm0720@gmail.com>
2025-05-23 15:36:53 -0500

    Update deleteAccount.js

--- api/deleteAccount.js
+++ api/deleteAccount.js
@@ -1,6 +1,10 @@
 const serviceRoleKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnemZ3aGtqamt4d3NodXB3ZHNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDMwMzY0NywiZXhwIjoyMDU5ODc5NjQ3fQ.MRTSJM37ZMqG8ixq948xebNfRNc-koV-K3bsiMh2Dt0";
 const projectURL = "https://tgzfwhkjjkxwshupwdse.supabase.co";
 
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(SUPABASE_URL, SERVICE_ROLE_KEY);
+
 module.exports = async (req, res) => {
   const { userID } = req.body;
 
